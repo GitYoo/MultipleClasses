b5b95a8f03f486a7e8c4e2800787f042b334a9a0
GitYoo <yoojds@gmail.com>
2019-02-06 11:02:15 -0500

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -5,9 +5,10 @@ package mainclass;
 public class MainClass {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {     //Hey what this mean?
         SubClass class1 = new SubClass();
         class1.simpleMessage();
     }
     
 }
+
